5d571db1e65257d7a50b33383001083f693e5185
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-12-16 18:34:26 +0000

    chore: update dependencies
    BREAKING CHANGE:
    - The minimum version is Node.js v16

--- __tests__/fts-client.spec.ts
+++ __tests__/fts-client.spec.ts
@@ -2,7 +2,6 @@ import { server } from '@test/fts.mock'
 import { FTSClient } from '@src/fts-client'
 import { TOKEN } from '@test/utils'
 import '@blackglory/jest-matchers'
-import 'jest-extended'
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 beforeEach(() => server.resetHandlers())
